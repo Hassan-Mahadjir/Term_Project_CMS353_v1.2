f06f261df1d903a4b52de74169c2bd2d3fe91eed
Hassan-Mahadjir <133597308+Hassan-Mahadjir@users.noreply.github.com>
2023-12-12 12:23:44 +0200

    update form

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String,Date,ForeignKey
 from datetime import datetime
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TeamsDataBase.db'
@@ -20,12 +21,15 @@ chat=db.Table('chat',db.Column('ann_id',db.Integer,db.ForeignKey('announcement.a
                          )
 
 
-class Admin(db.Model):
+class Admin(UserMixin, db.Model):
     ad_id = db.Column(db.Integer, primary_key=True)
     ad_name = db.Column(db.String(50), nullable=False)
     ad_email = db.Column(db.String(), nullable=False)
     ad_password = db.Column(db.String(), nullable=False)
     instructors = db.relationship('Instructor', backref='Admin')
+
+    def get_id(self):
+           return (self.ad_id)
 
 class Instructor(db.Model):
     inst_id = db.Column(db.Integer, primary_key=True)
@@ -34,7 +38,6 @@ class Instructor(db.Model):
     inst_password = db.Column(db.String(), nullable=False)
     admin_id = db.Column(db.Integer, db.ForeignKey('admin.ad_id'))
 
-    # students = db.relationship('Student', backref='Instructor')
     groups = db.relationship('Group', backref='Instructor')
     announcements = db.relationship('Announcement', backref='Instructor')
 
@@ -44,7 +47,6 @@ class Student(db.Model):
     std_name = db.Column(db.String(50), nullable=False)
     std_email = db.Column(db.String(), nullable=False)
     std_password = db.Column(db.String(), nullable=False)
-    # instructor_id = db.Column(db.Integer, db.ForeignKey('instructor.inst_id'))
     teaching=db.relationship('Instructor',secondary=student_instructor, backref='teachers')
 
 
@@ -53,7 +55,6 @@ class Group(db.Model):
     grp_id = db.Column(db.Integer, primary_key=True)
     grp_name = db.Column(db.String(50), nullable=False)
     instructor_id = db.Column(db.Integer, db.ForeignKey('instructor.inst_id'))
-    # groups = db.relationship('Instructor', backref='Group')
     grouping=db.relationship('Student',secondary=classes, backref='groupers')
 
 
@@ -73,29 +74,7 @@ class Announcement(db.Model):
 
 # with app.app_context():
 #      db.create_all()
-# admin=Admin(ad_id='1',ad_name='Nour',ad_password='barakat',ad_email='nour@gmail.com')
-instructor=Instructor(inst_id=2,inst_name='Nada',inst_password='Kollah',inst_email='nada@gmail.com',admin_id=1)
-instructor=Instructor(inst_id=3,inst_name='Alex',inst_password='russian',inst_email='alex@gmail.com',admin_id=1)
 
-student=Student(std_id=1,std_name='Ahmed',std_password='xyz',std_email='ahmed@gmail.com')
-student2=Student(std_id=2,std_name='hasan',std_password='zyx',std_email='hasan@gmail.com',instructor_id=2)
-student3=Student(std_id=3,std_name='nada',std_password='xzy',std_email='n@gmail.com',instructor_id=2)
+# with app.app_context():
 
-group=Group(grp_id=1,grp_name='CMSE353',instructor_id=1)
-group1=Group(grp_id=2,grp_name='CMSE354',instructor_id=2)
-group2=Group(grp_id=3,grp_name='CMSE473',instructor_id=1)
-
-channel=Channel(ch_id=1,ch_name='General',group_id=2)
-channel1=Channel(ch_id=2,ch_name='Lab',group_id=2)
-channel2=Channel(ch_id=3,ch_name='Tutorial',group_id=1)
-
-announcement= Announcement(ann_id=1,ann_title='Term Project',ann_body='Hello world',instructor_id=1)
-announcement1= Announcement(ann_id=2,ann_title='Term Project2',ann_body='Hello ',instructor_id=1)
-announcement2= Announcement(ann_id=3,ann_title='Term Project3',ann_body='Hi',instructor_id=2)
-
-student=Student(std_id=4,std_name='Athony',std_password='xxx',std_email='anthony@gmail.com',instructor_id=1)
-
-
-with app.app_context():
-    group.grouping.append(student)
-    db.session.commit()
+#     db.session.commit()
--- main.py
+++ main.py
@@ -1,62 +1,54 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
+from database import *
+from flask_login import login_user, LoginManager, current_user, logout_user
+from functools import wraps
+
+
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ourdatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TeamsDataBase.db'
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 db = SQLAlchemy()
 db.init_app(app)
 
+# Configure Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50),nullable=False)
-    email = db.Column(db.String(120),nullable=False)
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(Admin, user_id)
 
-# to put the tables in db file
-# with app.app_context():
-#     db.create_all()
-
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
- 
 with app.app_context():
     db.create_all()
 
-with app.app_context():
-    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
-    db.session.add(new_book)
-    db.session.commit()
 
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # If id is not 1 then return abort with 403 error
+        if current_user.ad_id != 1:
+            return abort(403)
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
 
-with app.app_context():
-    user = User(id=1,name='Hassaan', email='hahajaj')
-    db.session.add(user)
-    db.session.commit()
+    return decorated_function
 
-static = {"firstName": "name",
-            "lastName": "lastname",
-            "userName": "userName",
-            "email": "email@at.com",
-            "password": "password",
-            "type": "type"
-            }
 
 @app.route('/')
 def home():
-    return render_template("instructor_page.html")
+    return redirect(url_for('signin'))
 
 @app.route('/home_student')
 def home_student():
     return render_template("student_page.html")
 
 @app.route('/register', methods=['POST', 'GET'])
+@admin_only
 def register():
     if request.method == "POST":
         result = {"firstName": request.form['firstName'],
@@ -71,26 +63,42 @@ def register():
 
     return render_template('addForm.html')
 
+
 @app.route('/signin', methods=['POST', 'GET'])
 def signin():
 
     if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        
+        admin = db.session.execute(db.select(Admin).where(Admin.ad_id == 1)).scalar()
 
-        result1 = {"email": request.form["email"],
-                  "password": request.form['password']}
+        if email == admin.ad_email and password == admin.ad_password:
 
-        if result1["email"] == static["email"] and result1["password"] == static["password"]:
-            return redirect(url_for('home_student'))
+            login_user(admin)
+
+            return redirect(url_for("admin"))
         else:
-            return render_template('signin.html')
+            return redirect(url_for("signin"))
 
     return render_template('signin.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    print("logout done")
+    return redirect(url_for('signin'))
+
+@app.route('/admin')
+def admin():
+    users = db.session.execute(db.select(Student)).scalars().all()
+    print(users[0].std_email)
+    return render_template('admin_page.html', users= users)
 
 
 @app.route('/group')
 def group():
     return redirect(url_for('home'))
 
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
